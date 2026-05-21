1513a768d03becb50343d6e6060d08715d82b414
Phabiodnz <phabiodr@hotmail.com>
2026-05-21 16:59:03 -0300

    Função de verificação de nome repetido e inicio do cadastro de operador

--- centralDeOperacoes.c
+++ centralDeOperacoes.c
@@ -17,6 +17,32 @@ struct Equipamento{
 struct Operador listaOperadores[TAM_LISTA];
 struct Equipamento listaEquipamentos[TAM_LISTA];
 
+int qtdOperadoresCadastrados = 0;
+
+int verificadorNomeRepetido(char nome[]){
+    if(qtdOperadoresCadastrados != 0){
+        for(int i = 0; i < qtdOperadoresCadastrados; i++){
+            if(strcmp(nome, listaOperadores[i].nomeOperador) == 0){
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void cadastroOperador(){
+    int verificador = 0;
+    int idOperadorTemporario,qtdOperacoesTemporario;
+    char nomeOperadorTemporario[100],setorOperadorTemporario[4],nivelOperacionalTemporario[20],statusOperadorTemporario[10];
+
+    do{
+        printf("Nome: ");
+        fgets("%s",100,&nomeOperadorTemporario);
+
+        verificador = verificadorNomeRepetido(nomeOperadorTemporario);
+    }while(verificador == 0);
+}
+
 int main(){
 
 
